c8e8b31cab1c8f4d8a046a0b4070e0d43f012d95
Luke Wang <luke.wang@shearwaterasia.com>
2025-05-03 23:43:11 +0800

    move 3rd party auth to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,49 @@
-import LoginScreen from "@/components/LoginScreen"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { SignedIn, SignedOut, RedirectToSignIn, SignInButton } from '@clerk/clerk-react';
 
-export const BASE_URL = "http://localhost:4000/api/"
-const queryClient = new QueryClient();
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import LoginScreen from './components/LoginScreen';
+
+export const BASE_URL = "http://localhost:4000/api/" 
 
 function App() {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <div>
-          <LoginScreen />
-        </div>
-      </QueryClientProvider>
-    </>
+        <BrowserRouter>
+      <Routes>
+        {/* Public route */}
+        <Route path="/" element={
+          <SignInButton>
+            Sign In
+          </SignInButton>
+        } />
+
+        {/* Protected route */}
+        <Route
+          path="/main"
+          element={
+            <>
+              <SignedIn>
+                <LoginScreen />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+{/* <Box textAlign="center" py={10} px={6}>
+<Heading as="h1" size="2xl" mb={4}>
+  Welcome to My App
+</Heading>
+<Text fontSize="xl" mb={8}>
+  This is the main page of the application.
+</Text>
+
+</Box> */}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,31 @@
-import { Provider } from "@/components/ui/provider"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { Provider } from "@/components/ui/provider"
+
+import { ClerkProvider } from '@clerk/clerk-react'
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* clerk */}
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      {/* chakra */}
       <Provider>
         <App />
       </Provider>
+    </ClerkProvider>
   </StrictMode>,
 )
+
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+// const queryClient = new QueryClient();
+{/* <QueryClientProvider client={queryClient}>
+</QueryClientProvider> */}
+// import Login from './components/login.tsx'
